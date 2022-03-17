06d4c596480e595fe48bb4efb394ee3716f5b625
Uptight <RobloxArchiver@protonmail.com>
2022-03-17 00:03:44 -0400

    New Shit

--- Load.lua
+++ Load.lua
@@ -1,3 +1,13 @@
+--[[
+
+_G.Drops = value | Amount of drops
+_G.TermProcess = bool | When you finish dropping
+_G.SendChat = bool | Sends a start and leave message. 
+
+]]
+
 _G.Drops = 1
+_G.TermProcess = false
+_G.SendChat = true
 
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/RobloxArchiver/GhettoDropper/main/src/drop.lua"))()
